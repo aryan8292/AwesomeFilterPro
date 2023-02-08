6a8e50e313cb6b18db4d3f450979fe4849da11d8
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2023-02-08 22:39:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://shorturllink.in/api'
+    url = f'https://omegalinks.in/api'
     params = {'api': SHORTENER_API,
               'url': link,
               }
